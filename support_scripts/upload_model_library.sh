4fb160ebb0be3d6413824bd443e9338943816317
kikobb <xbarna02@stud.fit.vutbr.cz>
2020-07-24 00:01:48 +0200

    ignore shell error output

--- support_scripts/upload_model_library.sh
+++ support_scripts/upload_model_library.sh
@@ -15,14 +15,14 @@ case $1 in
     # OpenVino app
     -o)
         sshpass -p "root" scp -r -P 22 /home/k/PycharmProjects/face-detection/model_library/* \
-                                root@172.17.0.2:/home/openvino/face/models/
+                                root@172.17.0.2:/home/openvino/face/models
         ;;
     -pi4)
         sshpass -p "root" scp -r -P 22 /home/k/PycharmProjects/face-detection/model_library \
-                                root@192.168.0.206:/root/face-detection/
+                                root@192.168.0.206:/root/face-detection
         ;;
     -pi3)
         sshpass -p "root" scp -r /home/k/PycharmProjects/face-detection/model_library \
-                                root@192.168.0.207:/root/face-detection/
+                                root@192.168.0.207:/root/face-detection
         ;;
 esac
